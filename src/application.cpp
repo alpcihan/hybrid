68133e878b8463d4bc7163c1c8a8a14f7f9eb2e8
alpcihan <alpcihan.ac@gmail.com>
2023-12-30 23:22:28 +0100

    update default camera controller position

--- src/application.cpp
+++ src/application.cpp
@@ -28,6 +28,7 @@ void Application::run() {
 
     Timer timer;
     while (!m_tgai.windowShouldClose(*m_window)) {
+        std::cout << 1 / m_deltaTime << "\n";
         m_deltaTime = timer.elapsed();
         timer.reset();
         
