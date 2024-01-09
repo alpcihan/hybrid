ab9b0573f35c3e8a305ea25a3673fd5283a4bdcc
umut ercan <umuterc123@gmail.com>
2024-01-09 03:37:09 +0100

    add sdf soft shadows

--- shaders/glsl/core/ray_march_test.glsl
+++ shaders/glsl/core/ray_march_test.glsl
@@ -95,4 +95,20 @@ float shadow( in vec3 ro, in vec3 rd, float mint, float maxt )
 
 }
 
+// https://iquilezles.org/articles/rmshadows/
+float softshadow( in vec3 ro, in vec3 rd, float mint, float maxt, float w )
+{
+    float res = 1.0;
+    float t = mint;
+    for( int i=0; i<RAY_MARCH_MAX_ITERATION && t<maxt; i++ )
+    {
+        float h = map(ro + t*rd).x;
+        res = min( res, h/(w*t) );
+        t += clamp(h, 0.005, 0.50);
+        if( res<-1.0 || t>maxt ) break;
+    }
+    res = max(res,-1.0);
+    return 0.25*(1.0+res)*(1.0+res)*(2.0-res);
+}
+
 #endif
