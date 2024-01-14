c23b512409b00ec673c6643c6ed1dd01de74bcdb
GabrielePrinciotta98 <gabriele.princiotta98@gmail.com>
2024-01-14 15:12:50 +0100

    add MengerSponge fractal variation

--- shaders/glsl/core/sdf.glsl
+++ shaders/glsl/core/sdf.glsl
@@ -24,5 +24,25 @@ float sdRoundBox(in vec3 p, in vec3 b, float r) {
 float sdZPlane(in vec3 p, in float y) {
     return p.y - y;
 }
+// https://iquilezles.org/articles/menger/
+float sdMengerSponge( in vec3 p ) {
+    float d = sdBox(p,vec3(1.0));
+    
+    float s = 1.0;
+    for( int m=0; m<4; m++ ){
+        vec3 a = mod( (p - vec3(m+1, m+1, m+1)) *s, 2.0 )-1.0;
+        s *= 3.0;
+        vec3 r = abs(1.0 - 3.0*abs(a));
+
+        float da = max(r.x,r.y);
+        float db = max(r.y,r.z);
+        float dc = max(r.z,r.x);
+        float c = (min(da,min(db,dc))-1.0)/s;
+
+        d = max(d,c);
+    }
+
+    return d;
+}
 
 #endif
